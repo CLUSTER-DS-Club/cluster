c9d171539c884ceb668eaaac81ccab09b29da434
Tanmay Kalra <tanmaykalra0910@gmail.com>
2025-06-18 04:15:02 +0530

    Terms Page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Publications from './components/Publications.jsx'
 import ContactPage from './components/contact/ContactPage.jsx'
 import Privacy from './pages/Privacy/Privacy.jsx'
 import Cookies from './pages/Cookies/Cookies.jsx'
+import Terms from './pages/Terms of Service/Terms.jsx'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -44,6 +45,7 @@ function App() {
               <Route path="/publications" element={<Publications />} />
               <Route path='/privacy' element={<Privacy/>}/>
               <Route path='/cookies' element={<Cookies/>}/>
+              <Route path='/terms' element={<Terms/>}/>
 
             </Routes>
           </main>
