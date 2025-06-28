7654f0f0f258c4402800b66169daba8868592dd9
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-06-28 14:53:09 +0530

    worked on reviwed comments

--- src/pages/Disclaimer/Disclaimer.jsx
+++ src/pages/Disclaimer/Disclaimer.jsx
@@ -10,38 +10,27 @@ function Disclaimer() {
     <div className="disclaimer-container">
       <h1 className="disclaimer-title">Disclaimer</h1>
       <p className="disclaimer-text">
-        The information provided on this website is for general informational
-        purposes only. All content is provided in good faith; however, we make
-        no representations or warranties of any kind, express or implied,
-        regarding the accuracy, adequacy, validity, or completeness of any
-        information.
+         The information provided on this website is for general informational purposes only. All content is provided in good faith; however, we make no representations or warranties of any kind, express or implied, regarding the accuracy, adequacy, validity, or completeness of any information.
       </p>
 
       <h2 className="disclaimer-subtitle">No Professional Advice</h2>
       <p className="disclaimer-text">
-        The content on this site does not constitute professional advice (legal,
-        medical, financial, etc.). You should consult the appropriate
-        professionals before acting on any information provided.
+       The content on this site does not constitute professional advice (legal, medical, financial, etc.). You should consult the appropriate professionals before acting on any information provided.
       </p>
 
       <h2 className="disclaimer-subtitle">Limitation of Liability</h2>
       <p className="disclaimer-text">
-        Under no circumstance shall the CLUSTER team be held liable for any loss
-        or damage incurred as a result of using the site or relying on the
-        information provided.
+       Under no circumstance shall the CLUSTER team be held liable for any loss or damage incurred as a result of using the site or relying on the information provided.
       </p>
 
       <h2 className="disclaimer-subtitle">External Links Disclaimer</h2>
       <p className="disclaimer-text">
-        This site may contain links to external websites. We do not guarantee
-        the accuracy, relevance, or safety of any external content and are not
-        responsible for it.
+       This site may contain links to external websites. We do not guarantee the accuracy, relevance, or safety of any external content and are not responsible for it..
       </p>
 
       <h2 className="disclaimer-subtitle">Changes to This Disclaimer</h2>
       <p className="disclaimer-text">
-        We may update this disclaimer at any time without notice. You are
-        advised to review it periodically.
+       We may update this disclaimer at any time without notice. You are advised to review it periodically.
       </p>
 
       <h2 className="disclaimer-subtitle">Contact Information</h2>
