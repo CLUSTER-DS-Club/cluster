f10e6bb1203a6efdbd48702549fcc8d7140d0fa6
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-06-28 15:03:58 +0530

    fixed

--- src/pages/Disclaimer/Disclaimer.jsx
+++ src/pages/Disclaimer/Disclaimer.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import "./Disclaimer.css";
+import React, { useEffect } from 'react';
+import './Disclaimer.css';
 
 function Disclaimer() {
   useEffect(() => {
